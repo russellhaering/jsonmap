86599b2d85d294f51b50b8f38dc13ea834e18d1b
Russell Haering <russellhaering@gmail.com>
2015-02-03 00:05:19 -0800

    More coverage improvements

--- jsonmap_test.go
+++ jsonmap_test.go
@@ -22,7 +22,8 @@ var InnerThingTypeMap = TypeMap{
 		{
 			StructFieldName: "Foo",
 			JSONFieldName:   "foo",
-			Validator:       String(1, 255),
+			Validator:       String(1, 12),
+			Optional:        true,
 		},
 		{
 			StructFieldName: "AnInt",
@@ -82,5 +83,82 @@ func TestValidateOuterThing(t *testing.T) {
 	}
 	if v.Bar != "bazam" {
 		t.Fatal("Outer field Bar does not have expected value 'bazam':", v.Bar)
+	}
+}
+
+func TestValidateUnregisteredThing(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Fatal("No panic")
+		}
+	}()
+	v := &UnregisteredThing{}
+	TestTypeMapper.Unmarshal([]byte(`{}`), v)
+	t.Fatal("Unexpected success")
+}
+
+func TestValidateStringTypeMismatch(t *testing.T) {
+	v := &InnerThing{}
+	err := TestTypeMapper.Unmarshal([]byte(`{"foo": 12.0}`), v)
+	if err == nil {
+		t.Fatal("Unexpected success")
+	}
+	if err.Error() != "error validating field 'foo': not a string" {
+		t.Fatal("Unexpected error message:", err.Error())
+	}
+}
+
+func TestValidateStringTooShort(t *testing.T) {
+	v := &InnerThing{}
+	err := TestTypeMapper.Unmarshal([]byte(`{"foo": ""}`), v)
+	if err == nil {
+		t.Fatal("Unexpected success")
+	}
+	if err.Error() != "error validating field 'foo': too short, must be at least 1 characters" {
+		t.Fatal("Unexpected error message:", err.Error())
+	}
+}
+
+func TestValidateStringTooLong(t *testing.T) {
+	v := &InnerThing{}
+	err := TestTypeMapper.Unmarshal([]byte(`{"foo": "thisvalueistoolong"}`), v)
+	if err == nil {
+		t.Fatal("Unexpected success")
+	}
+	if err.Error() != "error validating field 'foo': too long, may not be more than 12 characters" {
+		t.Fatal("Unexpected error message:", err.Error())
+	}
+}
+
+func TestValidateBooleanTypeMismatch(t *testing.T) {
+	v := &InnerThing{}
+	err := TestTypeMapper.Unmarshal([]byte(`{"a_bool": 12.0}`), v)
+	if err == nil {
+		t.Fatal("Unexpected success")
+	}
+	if err.Error() != "error validating field 'a_bool': not a boolean" {
+		t.Fatal("Unexpected error message:", err.Error())
+	}
+}
+
+func TestValidateIntegerTypeMismatch(t *testing.T) {
+	v := &InnerThing{}
+	err := TestTypeMapper.Unmarshal([]byte(`{"an_int": false}`), v)
+	if err == nil {
+		t.Fatal("Unexpected success")
+	}
+	if err.Error() != "error validating field 'an_int': not an integer" {
+		t.Fatal("Unexpected error message:", err.Error())
+	}
+}
+
+func TestValidateIntegerNumericTypeMismatch(t *testing.T) {
+	v := &InnerThing{}
+	err := TestTypeMapper.Unmarshal([]byte(`{"an_int": 12.1}`), v)
+	if err == nil {
+		t.Fatal("Unexpected success")
+	}
+	if err.Error() != "error validating field 'an_int': not an integer" {
+		t.Fatal("Unexpected error message:", err.Error())
 	}
 }
